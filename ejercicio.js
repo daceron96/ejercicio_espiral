35fccbe10462da413c874d4129fdd177d09b613f
daceron96 <daceron96@gmail.com>
2023-01-12 22:34:57 -0500

    refactorizacion de codigo

--- ejercicio.js
+++ ejercicio.js
@@ -2,34 +2,28 @@ const ejercicio = () => {
   let size = 1003;
   let maxIteration = Math.floor(size / 2)
   //suma diagonal (0,n) - (n,0)
-  let sum1 = sumLeft(maxIteration, 4) + sumRigth(maxIteration, 8)
-  //suma de diagona (0,0) - (n,n)
-  let sum2 = sumLeft(maxIteration, 6) -1 + sumRigth(maxIteration, 2) 
+  let sum1 = sumDiagonal(maxIteration, 4, 8 )
+  //suma de diagonal (0,0) - (n,n)
+  let sum2 = sumDiagonal(maxIteration, 6, 2 ) - 1
   console.log(sum1 + sum2);
 
 };
-const sumLeft = (maxIteration, initial) => {
-  let previous = 1;
-  let next = initial;
+
+const sumDiagonal = (maxIteration, initial1, initial2) =>{
+  let previous1 = 1;
+  let next1 = initial1
+  let previous2 = 1;
+  let next2 = initial2;
   let sum = 1;
-  for (let i = 1; i <= maxIteration; i++) {
-    previous += next
-    sum += previous
-    next += 8
-  }
-  return sum
-};
-const sumRigth = (maxIteration, initial) => {
-  let previous = 1
-  let next = initial ;
-  let sum = 0;
 
   for (let i = 1; i <= maxIteration; i++) {
-    previous += next
-    sum += previous
-    next += 8
+    previous1 += next1;
+    sum += previous1;
+    next1 += 8;
+    previous2 += next2;
+    sum += previous2;
+    next2 += 8;
   }
-  return sum
-};
+}
 
 ejercicio()
